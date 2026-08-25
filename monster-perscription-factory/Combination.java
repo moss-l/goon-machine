7b27873bf77579e6383e34039136f1e8c6986b86
lairdschiller <lairdschiller@students.berkeley.net>
2026-08-25 18:26:22 +0000

    Ghoon

---
+++ monster-perscription-factory/Combination.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Combination {
+
+    private String ingredients;
+    
+    public void mix(Combination second) {
+        ArrayList<String> secondIngredients = new ArrayList<>();
+        
+    }
+
+}
